5fc1021c41a7253a1cc066932ea5767b0a5a4cd9
Pat52-byte <giampala@gmail.com>
2025-05-29 15:49:40 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 import torch
 import torchaudio
-from transformers import Wav2Vec2Processor, Wav2Vec2ForSequenceClassification
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForSequenceClassification
 
 MODEL_ID = "dima806/english_accents_classification"
 
-processor = Wav2Vec2Processor.from_pretrained(MODEL_ID)
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(MODEL_ID)
 model = Wav2Vec2ForSequenceClassification.from_pretrained(MODEL_ID)
 
 def predict_accent(file_path):
@@ -13,12 +13,13 @@ def predict_accent(file_path):
         resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)
         speech = resampler(speech)
 
-    input_values = processor(speech[0], sampling_rate=16000, return_tensors="pt").input_values
+    inputs = feature_extractor(speech[0], sampling_rate=16000, return_tensors="pt")
+    input_values = inputs.input_values
+
     with torch.no_grad():
         logits = model(input_values).logits
         predicted_id = torch.argmax(logits, dim=-1).item()
-        score = torch.nn.functional.softmax(logits, dim=-1).squeeze()[predicted_id].item()
+        confidence = torch.nn.functional.softmax(logits, dim=-1)[0][predicted_id].item()
     
-    predicted_label = model.config.id2label[predicted_id]
-    return predicted_label, score
-
+    label = model.config.id2label[predicted_id]
+    return label, confidence
